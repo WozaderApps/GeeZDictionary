e9858b9c66ffc9184115196ae6ceef5a67547a11
Yimesgen Morka <webDesigner299@gmail.com>
2019-01-09 17:27:36 +0300

    initial commit

--- app/src/main/java/tech/cursor/com/geezdictionary/MainActivity.java
+++ app/src/main/java/tech/cursor/com/geezdictionary/MainActivity.java
@@ -7,8 +7,10 @@ public class MainActivity extends AppCompatActivity {
 
     //  ***Wozader Apps***
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
