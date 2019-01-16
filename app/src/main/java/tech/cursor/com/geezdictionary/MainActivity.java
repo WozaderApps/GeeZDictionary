aa76d3b212f9bdd130cb8e1d1878705ba33bbaed
Wozader Apps <Learningpurpose299@gmail.com>
2019-01-16 12:56:03 +0300

    migrating to android 3.3 and File->Invalidate Caches/Restart on Project

--- app/src/main/java/tech/cursor/com/geezdictionary/MainActivity.java
+++ app/src/main/java/tech/cursor/com/geezdictionary/MainActivity.java
@@ -1,7 +1,7 @@
 package tech.cursor.com.geezdictionary;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
